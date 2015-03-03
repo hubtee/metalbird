173683d4ac4d284a3b661c5e4ee68324b02dc0ed
nacyot <propellerheaven@gmail.com>
2015-03-04 00:20:56 +0900

    Add authentication class and spec

--- spec/twitter_publisher/authentication_spec.rb
+++ spec/twitter_publisher/authentication_spec.rb
@@ -1,7 +1,41 @@
 require 'spec_helper'
 
 describe TwitterPublisher::Authentication do
-  it 'First Test' do
+  let(:opts) do
+    opts = {}
+    opts[:consumer_key] = ''
+    opts[:consumer_secret] = ''
+    opts[:access_token] = ''
+    opts[:access_token_secret] = ''
+
+    opts
+  end
+
+  describe 'Initialize - Opts validate' do
+    it 'is fail when there isn\'t authentication key' do
+      expect do
+        TwitterPublisher::Authentication.new({})
+      end.to raise_error(ArgumentError)
+    end
+
+    it 'is success when there are all authentication keys' do
+      expect do
+        TwitterPublisher::Authentication.new(opts)
+      end.to_not raise_error
+    end
+  end
+
+  describe 'Initialize' do
+    let(:auth) do
+      TwitterPublisher::Authentication.new(opts)
+    end
     
+    it 'is set client object' do
+      expect(auth.client).to_not be_nil
+    end
+
+    specify 'Client object is a Twitter::Rest::Client instance' do
+      expect(auth.client).to be_kind_of(Twitter::REST::Client)
+    end
   end
 end
