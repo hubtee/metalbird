bc62fafb06ee51393db7f543f4bca438f23c26a7
nacyot <propellerheaven@gmail.com>
2015-03-05 02:04:38 +0900

    Add twitter/publisher spec

--- spec/post_publisher/twitter/publisher_spec.rb
+++ spec/post_publisher/twitter/publisher_spec.rb
@@ -1,7 +1,36 @@
 require 'spec_helper'
 
 describe PostPublisher::Twitter::Publisher do
-  it 'secondTest' do
-    
+  let(:auth) do
+    opts = {}
+    opts[:consumer_key] = ''
+    opts[:consumer_secret] = ''
+    opts[:access_token] = ''
+    opts[:access_token_secret] = ''
+
+    PostPublisher::Twitter::Authentication.new(opts)
+  end
+
+  let(:tweet){'Hello, world!'}
+  
+  describe 'publish' do
+    specify 'Publish return tweet object' do
+      publisher = PostPublisher::Twitter::Publisher.new(auth)
+      allow(publisher).to receive(:publish) { Twitter::Tweet.new(id: 0) }
+
+      result = publisher.publish(tweet)
+      expect(result).to be_an_instance_of(Twitter::Tweet)
+    end
+
+    it 'raise Forbidden error when publishing failed' do
+      publisher = PostPublisher::Twitter::Publisher.new(auth)
+      allow(publisher).to receive(:publish) do
+        raise Twitter::Error::Forbidden
+      end
+
+      expect do
+        publisher.publish(tweet)
+      end.to raise_error(Twitter::Error::Forbidden)
+    end
   end
 end
