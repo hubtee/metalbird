01714eaa2246820ad74f6a181118cc9e2a07f18e
nacyot <propellerheaven@gmail.com>
2015-09-19 19:47:10 +0900

    Implement publish argument class(tweet)

--- lib/post_publisher/twitter/publish_args.rb
+++ lib/post_publisher/twitter/publish_args.rb
@@ -1,11 +1,93 @@
+require 'twitter-text'
+
 module PostPublisher
   module Twitter
     class PublishArgs
-      attr_reader :tweet
+      MAX_TWEET_LENGTH = 140
+      IMAGE_LENGTH = 23
+      LINK_LENGTH = 23
+      MAX_LINK_COUNT = 4
+      MAX_IMAGE_COUNT = 4
+
+      attr_reader :tweet, :links, :images
 
       def initialize(data)
-        @tweet = data[:tweet]
+        fail NoTweetError unless data[:tweet]
+
+        analyzed_tweet = analyze_tweet(data[:tweet])
+
+        @tweet = analyzed_tweet[:tweet]
+        @links = (data[:links] || []) + analyzed_tweet[:links]
+        @images = data[:images] || []
+      end
+
+      def validate?
+        validate_not_empty &&
+          validate_image_count &&
+          validate_link_count &&
+          validate_length
+      end
+
+      def images?
+        true if @images.length > 0
+      end
+
+      def links?
+        true if @links.length > 0
+      end
+
+      def image_count
+        images.length
+      end
+
+      def link_count
+        links.length
+      end
+
+      private
+
+      def validate_not_empty
+        @tweet != ''
+      end
+
+      def validate_length
+        @tweet.length <= max_length 
+      end
+
+      def validate_image_count
+        image_count <= MAX_IMAGE_COUNT
+      end
+
+      def validate_link_count
+        link_count <= MAX_LINK_COUNT
+      end
+
+      def analyze_tweet(tweet)
+        urls = ::Twitter::Extractor.extract_urls(tweet)
+        ::Twitter::Extractor.extract_urls_with_indices(tweet).each do |url|
+          tweet.slice!(*url[:indices])
+        end
+
+        {
+          tweet: tweet.strip,
+          links: urls
+        }
+      end
+
+      def max_length
+        case
+        when images? && links?
+          return MAX_TWEET_LENGTH - IMAGE_LENGTH - LINK_LENGTH * link_count
+        when images? && !links?
+          return MAX_TWEET_LENGTH - IMAGE_LENGTH
+        when !images? && links?
+          return MAX_TWEET_LENGTH - LINK_LENGTH * link_count
+        else
+          return MAX_TWEET_LENGTH
+        end
       end
     end
+
+    class NoTweetError < StandardError; end
   end
 end
---
+++ spec/post_publisher/twitter/publish_args_spec.rb
@@ -0,0 +1,201 @@
+require 'spec_helper'
+
+describe PostPublisher::Twitter::PublishArgs do
+  subject { PostPublisher::Twitter::PublishArgs }
+
+  let(:tweet) { 'Hello, world!' }
+  let(:basic_args) { subject.new(tweet: tweet) }
+  let(:empty_args) { subject.new(tweet: '') }
+
+  let(:images_args) do
+    data = {
+      tweet: tweet,
+      images: ['Image1', 'Images2']
+    }
+
+    subject.new(data)
+  end
+
+  let(:links_args) do
+    subject.new(
+      tweet: tweet,
+      links: ['https://test.com', 'https://nacyot.com']
+    )
+  end
+
+  let(:links_images_args) do
+    subject.new(
+      tweet: tweet,
+      images: ['Image1', 'Images2'],
+      links: ['https://test.com', 'https://nacyot.com']
+    )
+  end
+
+  describe '#initialize' do
+    it "raise NoTweetError when there isn't tweet body" do
+      expect do
+        subject.new({})
+      end.to raise_error(PostPublisher::Twitter::NoTweetError)
+    end
+
+    it "'s links has empty when links param is empty" do
+      expect(basic_args.links).to be_empty
+    end
+
+    it "'s images has empty when links param is empty" do
+      expect(basic_args.images).to be_empty
+    end
+  end
+
+  describe '#validate?' do
+    context 'not empty' do
+      it 'returns true when tweet is not empty' do
+        expect(basic_args.validate?).to be_truthy
+      end
+
+      it 'returns false when tweet is empty' do
+        expect(empty_args.validate?).to be_falsey
+      end
+    end
+
+    context 'image_count' do
+      it "returns true when there isn't image" do
+        expect(basic_args.validate?).to be_truthy
+      end
+
+      it 'returns true when there are four images' do
+        count = subject::MAX_IMAGE_COUNT
+        data = { tweet: tweet, images: Array.new(count) { 'Image' } }
+        expect(subject.new(data).validate?).to be_truthy
+      end
+
+      it 'returns false when there are more than four images' do
+        count = subject::MAX_IMAGE_COUNT + 1
+        data = { tweet: tweet, images: Array.new(count) { 'Image' } }
+        expect(subject.new(data).validate?).to be_falsey
+      end
+    end
+
+    context 'link_count' do
+      it "returns true when there isn't link" do
+        expect(basic_args.validate?).to be_truthy
+      end
+
+      it 'returns true when there are four links' do
+        count = subject::MAX_LINK_COUNT
+        data = { tweet: tweet, links: Array.new(count) { 'Link' } }
+        expect(subject.new(data).validate?).to be_truthy
+      end
+
+      it 'returns false when there are more than four links' do
+        count = subject::MAX_LINK_COUNT + 1
+        data = { tweet: tweet, links: Array.new(count) { 'Link' } }
+        expect(subject.new(data).validate?).to be_falsey
+      end
+    end
+
+    context 'max_length' do
+      context 'no images and no links' do
+        it 'returns true when the tweet is 140 char' do
+          data = { tweet: 'a' * 140 }
+          expect(subject.new(data).validate?).to be_truthy
+        end
+
+        it 'returns true when the tweet is 141 char' do
+          data = { tweet: 'a' * 141 }
+          expect(subject.new(data).validate?).to be_falsey
+        end
+      end
+
+      context 'images and no links' do
+        it 'returns true when the tweet is 117 char' do
+          data = { tweet: 'a' * 117, images: ['IMAGE'] }
+          expect(subject.new(data).validate?).to be_truthy
+        end
+
+        it 'returns false when the tweet is 118 char' do
+          data = { tweet: 'a' * 118, images: ['IMAGE'] }
+          expect(subject.new(data).validate?).to be_falsey
+        end
+      end
+
+      context 'no images and 1 links' do
+        it 'returns true when the tweet is 117 char' do
+          data = { tweet: 'a' * 117, links: ['https://test.com'] }
+          expect(subject.new(data).validate?).to be_truthy
+        end
+
+        it 'returns false when the tweet is 118 char' do
+          data = { tweet: 'a' * 118, links: ['https://test.com'] }
+          expect(subject.new(data).validate?).to be_falsey
+        end
+      end
+
+      context 'no images and 2 links' do
+        it 'returns true when the tweet is 94 char' do
+          data = { tweet: 'a' * 94, links: ['https://test.com'] }
+          expect(subject.new(data).validate?).to be_truthy
+        end
+
+        it 'returns false when the tweet is 95 char' do
+          data = { tweet: 'a' * 95, links: ['https://test.com'] * 2 }
+          expect(subject.new(data).validate?).to be_falsey
+        end
+      end
+
+      context '1 images and 1 links' do
+        it 'returns true when the tweet is 94 char' do
+          data = {
+            tweet: 'a' * 94,
+            images: ['IMAGES'],
+            links: ['https://test.com']
+          }
+          expect(subject.new(data).validate?).to be_truthy
+        end
+
+        it 'returns false when the tweet is 95 char' do
+          data = {
+            tweet: 'a' * 95,
+            images: ['IMAGES'],
+            links: ['https://test.com']
+          }
+          expect(subject.new(data).validate?).to be_falsey
+        end
+      end
+    end
+  end
+
+  describe 'images?' do
+    it 'is true when there are images' do
+      expect(images_args.images?).to be_truthy
+    end
+
+    it "is false when there isn't image" do
+      expect(basic_args.images?).to be_falsey
+    end
+  end
+
+  describe 'links?' do
+    it 'is true when there are links' do
+      expect(links_args.links?).to be_truthy
+    end
+
+    it "is false when there isn't link" do
+      expect(basic_args.links?).to be_falsey
+    end
+  end
+
+  describe 'image_count' do
+    it 'is length of images attribute' do
+      expect(images_args.image_count).to be_equal(2)
+      expect(basic_args.image_count).to be_equal(0)
+    end
+  end
+
+  describe 'link_count' do
+    it 'is length of links attribute' do
+      expect(links_args.link_count).to be_equal(2)
+      expect(basic_args.link_count).to be_equal(0)
+    end
+  end
+end
