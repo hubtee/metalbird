9036d5bf5cdbc35fcbb0f84650cb54ce9466a16c
nacyot <propellerheaven@gmail.com>
2015-09-19 20:39:43 +0900

    Add retweet args spec

--- lib/post_publisher/twitter/retweet_args.rb
+++ lib/post_publisher/twitter/retweet_args.rb
@@ -4,8 +4,11 @@ module PostPublisher
       attr_reader :tweet_id
 
       def initialize(data)
+        fail NoTweetIDError unless data[:tweet_id]
         @tweet_id = data[:tweet_id]
       end
     end
+
+    class NoTweetIDError < StandardError; end
   end
 end
