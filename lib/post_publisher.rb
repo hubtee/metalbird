d0b544030df04965368541d6aa5d6b1d92d75bc9
nacyot <propellerheaven@gmail.com>
2015-09-19 17:38:17 +0900

    Add logger to post_publisher

--- lib/post_publisher.rb
+++ lib/post_publisher.rb
@@ -1,6 +1,10 @@
+require 'logger'
+
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
 module PostPublisher
   autoload :Twitter, 'post_publisher/twitter'
   autoload :Tumblr, 'post_publisher/tumblr'
+
+  Logger = ::Logger.new(STDOUT)
 end
