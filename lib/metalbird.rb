94325a48e7c83fd0223b72e6b2dfb5f2144acf8b
nacyot <propellerheaven@gmail.com>
2015-09-20 14:06:23 +0900

    Add UrlProcessor module

--- lib/metalbird.rb
+++ lib/metalbird.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 module Metalbird
   autoload :Twitter, 'metalbird/twitter'
   autoload :Tumblr, 'metalbird/tumblr'
+  autoload :UrlProcessor, 'metalbird/url_processor'
   autoload :Authenticator, 'metalbird/authenticator'
 
   Logger = ::Logger.new(STDOUT)
---
+++ lib/metalbird/url_processor.rb
@@ -0,0 +1,6 @@
+module Metalbird
+  module UrlProcessor
+    autoload :Default, 'metalbird/url_processors/default'
+    autoload :Google, 'metalbird/url_processors/google'
+  end
+end
