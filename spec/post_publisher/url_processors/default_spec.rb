ff45c76bbdc187d3fe0f8d09377a5f48df2f0cdc
nacyot <propellerheaven@gmail.com>
2015-09-20 14:41:56 +0900

    Add default link processor spec

---
+++ spec/post_publisher/url_processors/default_spec.rb
@@ -0,0 +1,20 @@
+module Metalbird
+  module UrlProcessor
+    describe Default do
+      subject { Default.new }
+      let(:url) { 'http://github.com/nacyot' }
+
+      describe '#generate' do
+        it 'returns the param' do
+          expect(subject.generate(url)).to be_equal(url)
+        end
+      end
+
+      describe '#expand' do
+        it 'returns the param' do
+          expect(subject.generate(url)).to be_equal(url)
+        end
+      end
+    end
+  end
+end
